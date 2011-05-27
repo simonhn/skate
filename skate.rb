3da84cc261369830832ef8175be3f0b020265b69
simonhn <simonhn@gmail.com>
2011-05-27 20:46:27 +1000

    design changes

--- skate.rb
+++ skate.rb
@@ -73,11 +73,11 @@ configure do
   #setup MySQL connection:  
   
   #DataMapper::Logger.new('log/datamapper.log', :debug)
-  DataMapper.setup(:default, ENV['DATABASE_URL'] || 'mysql://pav.db')
+  #DataMapper.setup(:default, ENV['DATABASE_URL'] || 'mysql://pav.db')
   
-  #@config = YAML::load( File.open( 'config/settings.yml' ) )
-  #@connection = "#{@config['adapter']}://#{@config['username']}:#{@config['password']}@#{@config['host']}/#{@config['database']}";
-  #DataMapper.setup(:default, @connection)
+  @config = YAML::load( File.open( 'config/settings.yml' ) )
+  @connection = "#{@config['adapter']}://#{@config['username']}:#{@config['password']}@#{@config['host']}/#{@config['database']}";
+  DataMapper.setup(:default, @connection)
   DataMapper.auto_upgrade!
   #drops table and rebuilds
   #DataMapper.auto_migrate!
@@ -91,6 +91,7 @@ end
 get '/' do
   cache_control :public, :max_age => 600
   @route = Route.first
+  @spots = @route.spots
   haml :map
 end
 
